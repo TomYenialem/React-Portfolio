a61236e73b82200e8ec80060b06ae8729117fc72
TomYenialem <tom21g2008@gmail.com>
2025-01-04 14:52:24 +0300

    toggle functionality for menu page

--- src/Componenets/Header/Header.jsx
+++ src/Componenets/Header/Header.jsx
@@ -1,114 +1,104 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { RiMenu3Fill } from "react-icons/ri";
-import img from "../../assets/images/tom-sm.jpg";
-import First from "../First/First";
-import Icons from "./Icons/Icons";
-import logo from "../../assets/images/logo.jpg";
-import { useMyContext } from "../Contexts/Context";
-import { FaArrowAltCircleUp } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
+import logo from "../../assets/images/logo.jpg"; // Make sure to have your logo here
 
 export default function Header() {
-  const { day, handleToggle } = useMyContext();
-  const [show, setShow] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+
+ 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Handle closing the menu when a link is clicked
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className="header" id="Home">
       <div className="header_wrapper fixed-top">
-        <nav className="navbar navbar-expand-md text-white  p-0  ">
-          <div className="container ">
-            <div className="">
-              <a className="navbar-brand text-white" href="#">
-                <img src={logo} className="logo"/>
+        <nav className="navbar navbar-expand-md text-white p-0">
+          <div className="container">
+            <div>
+              <a className="navbar-brand text-white" href="#Home">
+                <img src={logo} className="logo" alt="Logo" />
               </a>
             </div>
 
+            {/* Toggle Button */}
             <div
-              className="navbar-toggler p-0 "
+              className="navbar-toggler p-0"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapsibleNavbar"
+              onClick={toggleMenu} // Toggle menu on button click
             >
-              <span className="menu" onClick={() => setShow((prev) => !prev)}>
-                {show ? (
-                  <RiMenu3Fill className="text-white fw-bold fs-1 " />
-                ) : (
+              <span className="menu-icon">
+                {isMenuOpen ? (
                   <IoMdClose className="text-white fw-bold fs-1" />
+                ) : (
+                  <RiMenu3Fill className="text-white fw-bold fs-1" />
                 )}
               </span>
             </div>
+
+            {/* Use the state to control the collapse behavior */}
             <div
-              className="collapse navbar-collapse top  "
+              className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}
               id="collapsibleNavbar"
             >
-              <ul className="navbar-nav m-auto  w-100 d-flex justify-content-end  m-auto">
+              <ul className="navbar-nav m-auto w-100 d-flex justify-content-end">
                 <li className="nav-item">
                   <a
                     className="nav-link text-danger text-hover-danger px-4 mb-2"
                     href="#Home"
+                    onClick={handleLinkClick} // Close menu when link is clicked
                   >
                     Home
                   </a>
                 </li>
                 <div className="border-btm"></div>
                 <li className="nav-item">
-                  <a className="nav-link px-4" href="#About">
+                  <a
+                    className="nav-link px-4"
+                    href="#About"
+                    onClick={handleLinkClick}
+                  >
                     About
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link  px-4" href="#Porto">
+                  <a
+                    className="nav-link px-4"
+                    href="#Porto"
+                    onClick={handleLinkClick}
+                  >
                     Portfolio
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link  px-4" href="#Skill">
-                    skills
+                  <a
+                    className="nav-link px-4"
+                    href="#Skill"
+                    onClick={handleLinkClick}
+                  >
+                    Skills
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link  px-4" href="#Contact">
-                    Contacts
+                  <a
+                    className="nav-link px-4"
+                    href="#Contact"
+                    onClick={handleLinkClick}
+                  >
+                    Contact
                   </a>
                 </li>
-                {/* <div onClick={handleToggle}>
-                {day ? <FaToggleOn /> : <FaToggleOff />}
-              </div> */}
               </ul>
             </div>
           </div>
         </nav>
-      </div>
-
-      <div className="profile">
-        <img src={img} alt="" className="tom-sm" />
-      </div>
-      <div className="title text-center text-white tom">
-        <h1>
-          TEMESGEN <span>YENIALME</span>
-        </h1>
-        <div className="animate_intro">
-          <span>Hello I'm</span>
-          <p>
-            <First />
-          </p>
-        </div>
-        <div className="cv">
-          <a href="#Contact">
-            <button className="btn ">Contact Me</button>
-          </a>
-          <a href="#Contact">
-            <button className="btn resume">Download cv</button>
-          </a>
-        </div>
-      </div>
-      <div>
-        <Icons />
-      </div>
-      <div className="top">
-        <a href="#Home">
-          <FaArrowAltCircleUp />
-        </a>
       </div>
     </div>
   );
